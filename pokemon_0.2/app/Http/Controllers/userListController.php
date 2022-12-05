3eed0ae59d9c5748fb7fe42f7ec4355c823d6f25
a-sumo <armandsumo@gmail.com>
2022-12-05 18:31:06 +0100

    Create userListController.php

---
+++ pokemon_0.2/app/Http/Controllers/userListController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\View;
+
+class userListController extends Controller
+{
+    // Fetch all users from user model
+    public function getUserList(){
+        $users =  DB::table('users')->get();
+        return View::make('leaderboard', compact('users'));
+    }
+
+}
