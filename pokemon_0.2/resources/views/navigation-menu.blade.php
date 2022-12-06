2c2fd46ec7d7f1541f72d35a3b8d17d32c96b6bb
a-sumo <armandsumo@gmail.com>
2022-12-06 11:54:15 +0100

    add pokedex nav menu

--- pokemon_0.2/resources/views/navigation-menu.blade.php
+++ pokemon_0.2/resources/views/navigation-menu.blade.php
@@ -17,6 +17,11 @@
                     </x-jet-nav-link>
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-jet-nav-link href="{{ route('pokedex') }}" :active="request()->routeIs('pokedex')">
+                        {{ __('Pokedex') }}
+                    </x-jet-nav-link>
+                </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-jet-nav-link href="{{ route('leaderboard') }}" :active="request()->routeIs('leaderboard')">
                         {{ __('Leaderboard') }}
                     </x-jet-nav-link>
