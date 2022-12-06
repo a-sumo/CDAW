c80c09fe569b905afceea339d05d2aa34b317ed8
a-sumo <armandsumo@gmail.com>
2022-12-06 11:52:56 +0100

    add datatable import

--- pokemon_0.2/resources/views/layouts/template.blade.php
+++ pokemon_0.2/resources/views/layouts/template.blade.php
@@ -9,11 +9,15 @@
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
-
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
-
+        <script
+        src="https://code.jquery.com/jquery-3.6.1.min.js"
+        integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ="
+        crossorigin="anonymous"></script>
+        <script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js">></script>
         <!-- Styles -->
+        <link rel="stylesheet" href="https://cdn.datatables.net/1.13.1/css/jquery.dataTables.min.css">
         @livewireStyles
     </head>
     <body class="font-sans antialiased">
@@ -38,8 +42,8 @@
             </main>
         </div>
 
+        @yield('script')
         @stack('modals')
-
         @livewireScripts
     </body>
 </html>
