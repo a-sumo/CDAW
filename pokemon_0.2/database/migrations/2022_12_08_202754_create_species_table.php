adc179a996cfd452ab3a9fedc0f1540adf161e7e
a-sumo <armandsumo@gmail.com>
2022-12-08 21:59:51 +0100

    add species migration

---
+++ pokemon_0.2/database/migrations/2022_12_08_202754_create_species_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('species', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('base_hp');
+            $table->unsignedBigInteger('base_attack');
+            $table->unsignedBigInteger('base_defense');
+            $table->unsignedBigInteger('base_speed');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('species');
+    }
+};
--- pokemon_0.2/resources/views/home.blade.php
+++ pokemon_0.2/resources/views/home.blade.php
@@ -1,2 +1,5 @@
 <x-app-layout>
+<div id="poke_container" class="poke-container grid grid-cols-4 gap-4"></div>
+<script type= "module" src="./js/homepage.js">
+</script>
 </x-app-layout>
